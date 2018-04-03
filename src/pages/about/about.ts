dafa7628b70fd0143ae054a53b304c36c46e4f2e
Angel Careaga <acareagag@gmail.com>
2018-04-03 14:20:41 -0500

    Limpieza de código :zap: / Documentación :memo:
    - Se limpio el código, se eliminaron métodos que no se utilizaban en el componente.
    - Implementación de OnInit y OnDestroy en los componentes.
    - Se ocultaron los scrollbar.
    - Se documento todo el código para mejor entendimiento.
    - Se corrigierón temas de ortografía.

---
+++ src/pages/about/about.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  /**
+   * Inicializa todas las variables a utilizar en about.
+   */
+  constructor() { }
+
+}
--- src/providers/mac-bluetooth/mac-bluetooth.ts
+++ src/providers/mac-bluetooth/mac-bluetooth.ts
@@ -2,61 +2,82 @@ import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Storage } from '@ionic/storage';
 
-/*
-  Generated class for the MacBluetoothProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
-
 @Injectable()
 export class MacBluetoothProvider {
 
 	constructor(private storage: Storage) {
-    
-  }
+	}
 
-  public insert(key: string, mac: MacBluetooth) {
-	  return this.save(key, mac);
-  }
+	/**
+	 * Guarda en memoria, invocando al método local 'save'.
+	 * @param key Recibe el identificador.
+	 * @param mac Recibe el valor.
+	 */
+	public insert(key: string, mac: MacBluetooth): Promise<any> {
+		return this.save(key, mac);
+	}
 
-  public update(key: string, mac: MacBluetooth) {
-	  return this.save(key, mac);
-  }
+	/**
+	 * Actualiza valor en memoria, invocando al método local 'save'.
+	 * @param key Recibe el identificador.
+	 * @param mac Recibe el valor.
+	 */
+	public update(key: string, mac: MacBluetooth): Promise<any> {
+		return this.save(key, mac);
+	}
 
-  public save(key: string, mac: MacBluetooth) {
-	  return this.storage.set(key, mac);
-  }
+	/**
+	 * Asigna valores en memoria, con ayuda del plugin 'storage'.
+	 * @param key Recibe el identificador.
+	 * @param mac Recibe el valor.
+	 */
+	public save(key: string, mac: MacBluetooth): Promise<any> {
+		return this.storage.set(key, mac);
+	}
 
-  public remove(key: string) {
-	  return this.storage.remove(key);
-  }
+	/**
+	 * Elimina el valor en memoria, con ayuda del plugin 'storage'.
+	 * @param key Recibe el identificador.
+	 */
+	public remove(key: string): Promise<any> {
+		return this.storage.remove(key);
+	}
 
-  public getAll() {
+	/** 
+	 * Genera una lista de direcciones MAC, consultando en la memoria, y estructurando los valores en
+	 * la clase MacBluetooth, almacenándolas en MacBluetoothList.
+	*/
+	public getAll(): Promise<any> {
 
-	  let macsBluetooth: MacBluetoothList[] = [];
+		let macsBluetooth: MacBluetoothList[] = [];
 
-	  return this.storage.forEach((value: MacBluetooth, key: string, iterationNumber: Number) => {
-		  let macBluetooth = new MacBluetoothList();
-		  macBluetooth.key = key;
-		  macBluetooth.macBluetooth = value;
-		  macsBluetooth.push(macBluetooth);
-	  })
-		  .then(() => {
-			  return Promise.resolve(macsBluetooth);
-		  })
-		  .catch((error) => {
-			  return Promise.reject(error);
-		  });
-  }
+		return this.storage.forEach((value: MacBluetooth, key: string, iterationNumber: number) => {
+			let macBluetooth = new MacBluetoothList();
+			macBluetooth.key = key;
+			macBluetooth.macBluetooth = value;
+			macsBluetooth.push(macBluetooth);
+		})
+			.then(() => {
+				return Promise.resolve(macsBluetooth);
+			})
+			.catch((error) => {
+				return Promise.reject(error);
+			});
+	}
 }
 
+/** 
+ * Clase auxiliar para el método local getAll.
+*/
 export class MacBluetooth {
 	name: string;
-	mac: string	;
+	mac: string;
 	active: boolean;
 }
 
+/** 
+ * Clase auxiliar para el método local getAll.
+*/
 export class MacBluetoothList {
 	key: string;
 	macBluetooth: MacBluetooth;
